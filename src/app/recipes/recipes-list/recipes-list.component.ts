d0c258dd72c384f6cd8f9be52a07e8f719bdef99
arpita-comeco <arpita.behura@comeco.com>
2022-02-10 21:08:25 +0100

    Working on recipe details and styling

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[]=[
+    new Recipe('sandwich','great for breakfast','https://media.istockphoto.com/photos/italian-style-panini-picture-id1292147140'),
+  ];
   constructor() { }
 
   ngOnInit(): void {
