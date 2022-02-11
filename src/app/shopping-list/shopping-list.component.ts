a57c96dcce35460ab67a9ce5a1a77aee1720c641
arpita-comeco <arpita.behura@comeco.com>
2022-02-11 16:19:52 +0100

    Creating Shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[]= [
+    new Ingredient('bread',3),
+    new Ingredient('cheese',4),
+    new Ingredient('sauce',1),
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
